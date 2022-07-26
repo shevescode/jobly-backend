5f21b0d52137b45e36f383ddb47a0109b396cd18
Wiktoria Rajba <w.rajba@befard.pl>
2022-07-26 13:04:13 +0200

    fixed cors problem during filling employer form

--- src/main/java/com/jobly/Jobly/controller/EmployerRestController.java
+++ src/main/java/com/jobly/Jobly/controller/EmployerRestController.java
@@ -6,14 +6,13 @@ import com.jobly.Jobly.model.user.User;
 import com.jobly.Jobly.service.EmployerService;
 import com.jobly.Jobly.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.PermitAll;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RequiredArgsConstructor
 public class EmployerRestController {
 
@@ -27,6 +26,8 @@ public class EmployerRestController {
 
     @PostMapping("/employer")
     public void createEmployer(@RequestBody EmployerDto employerDto) {
+        System.out.println("weszło tutaj");
+        System.out.println(employerDto.toString());
         Employer newEmployer = Employer.builder()
                 .user(userService.getByEmail(employerDto.getUserEmail()).orElseThrow())
                 .companyName(employerDto.getCompanyName())
